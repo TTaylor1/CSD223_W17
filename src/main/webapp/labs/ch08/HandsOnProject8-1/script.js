7613791508131f480776dc60518e3d013c14b14c
Tom <Tom@192.168.0.7>
2017-03-10 23:20:52 -0500

    Merge origin/master
    Conflicts:
    	pom.xml

---
+++ src/main/webapp/labs/ch08/HandsOnProject8-1/script.js
@@ -0,0 +1,59 @@
+   /*
+      JavaScript 6th Edition
+      Chapter 8
+      Hands-on Project 8-1
+
+      Author: Thomas Taylor
+      Date:   March 10,2017
+
+      Filename: script.js
+   */
+  
+  "use strict";
+  
+  var newAccountArray;
+
+function createID(){
+    var fname = document.getElementById("fnameinput");
+    var lname = document.getElementById("lnameinput");
+    var zip = document.getElementById("zipinput");
+    var account = document.getElementById("accountidbox");
+    var fields = document.getElementsByTagName("input");
+    var acctid;
+    var firstInit;
+    var lastInit;
+    
+    if (fname.value !== "" && lname.value !== "" && zip.value !== ""){
+        firstInit = fname.value.charAt(0).toUpperCase();
+        lastInit = lname.value.charAt(0).toUpperCase();
+        acctid = firstInit + lastInit + zip.value;
+        account.value = acctid;
+        newAccountArray = [];
+        for (var i = 0; i < fields.length - 1; i++){
+            newAccountArray.push(fields[i].value);
+        }
+    }
+    
+}
+
+function createEventListener(){
+    var fname = document.getElementById("fnameinput");
+    var lname = document.getElementById("lnameinput");
+    var zip = document.getElementById("zipinput");
+    
+    if(fname.addEventListener){
+        fname.addEventListener("change", createID, false);
+        lname.addEventListener("change", createID, false);
+        zip.addEventListener("change", createID, false);
+    } else if (fname.attachEvent){
+        fname.attachEvent("onchange", createID);
+        lname.attachEvent("onchange", createID);
+        zip.attachEVent("onchange", createID);
+    }
+}
+
+if(window.addEventListener){
+    window.addEventListener("load", createEventListener, false);
+}else if(window.attachEvent){
+    window.attachEvent("onload", createEventListener);
+}
